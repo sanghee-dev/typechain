46a8251d659e92e13cc442f2b63076f37dd38125
leeedev <leeesangheee@gmail.com>
2021-01-15 22:28:59 +0900

    Create Block class

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+export {};
